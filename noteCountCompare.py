cfd5acfc527014d5fbf6744f66f1f02d6af77a54
alexleclerc <aleclerc15@mail.wou.edu>
2017-07-31 10:19:15 -0400

    documentation update
    informative comments

--- noteCountCompare.py
+++ noteCountCompare.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 import numpy
 from numpy.ma import arange
 
+#chart comparing the amount of notes taken with the smart glasses on vs without
+
 #in a perfect world this would take a text or csv file but we don't live there so its hard coded.
 wglassesEasiness = [numpy.nan, 3.5, 5, numpy.nan, 4, 2, 3, 4, 2, 2, 5, 2.5, 3, 2, 2.5, 1]
 wglassesNotes = [0,85,34,0,64,156,61,20,70,102,57,89,40,8,77,62]
@@ -13,7 +15,6 @@ x = arange(16)
 #Plot with glasses and without glasses
 plt.scatter(x, wglassesNotes, label='with glasses')
 plt.scatter(x, woglassesNotes, marker='x', label ='without glasses')
-
 
 #formatting
 plt.axes().set_axisbelow(True)
